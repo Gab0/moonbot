0def12d995541f5075a7354e763b78dac4e93bc0
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-01-11 00:37:04 -0200

    .

---
+++ interface/cmdparser.py
@@ -0,0 +1,56 @@
+import optparse
+
+parser = optparse.OptionParser()
+parser.add_option('--watch', action='store_true', dest='Watch', default=False)
+parser.add_option('--nonoptimal', action='store_true',
+                  dest='NonOptimal', default=False)
+# DEBUG MODES;
+parser.add_option('--blankrun', action='store_true',
+                  dest='BlankRun', default=False,
+                  help="Moneybot exits before real time evaluations/transactions.")
+parser.add_option('--debug', action='store_true',
+                  dest='MajorDebug', default=False)
+parser.add_option('--noasync', action='store_false',
+                  dest='AsyncPool', default=True,
+                  help='Run brute force calibration on a single process'+\
+                          ' (debug purpose).')
+
+parser.add_option('--nocalibrate', action='store_true',
+                  dest='NoCalibrate', default=False)
+parser.add_option('--view', action='store_true',
+                  dest='ShowStartupCandlestick', default=False,
+                  help='Show recent cotation data on start up.')
+parser.add_option('--strict', action='store_true',
+                  dest='StrictMode', default=False)
+parser.add_option('--triple', action='store_true',
+                  dest='TripleSimultaneous', default=False)
+
+# CALIBRATION MODES;
+parser.add_option('-p', '--calibrate_population', action='store',
+                  type='int', dest='GeneticCalibration', default=None,
+                  help='Calibrate criterion using genetic algorithm.')
+parser.add_option('--calibrate_bruteforce', action='store_true',
+                  dest='BruteForceCalibration', default=False,
+                  help='Calibrate criterion using brute force (testing all possibilities).')
+parser.add_option('--save', action='store_true',
+                  dest='SaveCriterion', default=False,
+                  help="Save the criterion to calibrated on session")
+parser.add_option('--database', action='store_true',
+                  dest='CalibrateFromDatabase', default=False)
+
+parser.add_option('--flagbank', action='store_true',
+                  dest='FlagBankMode', default=False,
+                  help='Think on brain memory mode -- experimental')
+parser.add_option('--makeflagbank', action='store_true',
+                  dest='MakeFlagBank', default=False,
+                  help='Make or renew the flag bank. Flag bank mode depends on that.')
+
+parser.add_option('--sell', action='store_true',
+                  dest='ImmediateSell', default=False)
+
+# NOTIFICATOIN MODES;
+parser.add_option('-m', '--mail', action='store_true',
+                  dest='mail', default=False,
+                  help="Receive mail notifications on transactions. Need to setup credentials, read README")
+parser.add_option('-e', '--exchange', dest='exchanges', default='')
+(options, args) = parser.parse_args()
---
+++ interface/interface.py
@@ -0,0 +1,88 @@
+#!/bin/python
+import sys
+from time import sleep
+from colored import fg
+
+
+def showSleepBar(Time):
+    Length = 82
+    Interval = Time / 82
+
+    Fill = '.'
+    Void = ' '
+    Init = '|'
+    End = '|'
+
+    for k in range(Length):
+        v = Length-k
+        print('\r'+Init + k*Fill + v*Void + End, end="")
+        sys.stdout.flush()
+        sleep(Interval)
+    print("")
+
+
+def showRunningTime(starttime, now):
+    RunningTime = now - starttime
+    RTs = RunningTime.seconds
+    RTd = RunningTime.days
+    message = "running for %i days, " % RTd
+    message += "%i hours, " % (RTs % 86400 // 3600)
+    message += "%i minutes " % (RTs % 86400 % 3600 // 60)
+    message += "& %i seconds." % (RTs % 86400 % 3600 % 60)
+
+    print(message)
+
+
+def showTransactionCountStatistics(TransactionCount):
+    print("Limit Market Orders: ( total: %i  |  cancelled %i)" % (
+        TransactionCount['Created'], TransactionCount['Cancelled']))
+
+
+def showCoinHeader(Coin, varSessionCoinPrice, varEnterCoinPrice, CandlestickData):
+
+    # First line.
+    _color = 'purple_1b' if Coin.Active else 'blue'
+    mainHeader = "\t\tAnalyzing %s candlesticks; vol %.2f; t %.2f"
+    printColored(mainHeader % (Coin.MarketName,
+                               CandlestickData[-1][5], 0),
+                 _color, end='')
+
+    scoreColor = 'green' if Coin.TransactionScore >= 0 else "dark_violet_1b"
+    printColored("|> %.3f" % Coin.TransactionScore, scoreColor)
+
+    # Second line.
+    print('\t', end='')
+    for P in [varSessionCoinPrice, varEnterCoinPrice]:
+        _color = 'red' if P < 0 else 'green'
+        printColored("%.3f%%\t" %
+                     P, _color, end='')
+
+def showTransactionRecord(TransactionRecord):
+    print("Transaction Record:")
+    print()
+
+    printColored("Confirmed -> %i" % TransactionRecord['confirmed'], 'green')
+    printColored("Tried -> %i" % TransactionRecord['tried'], 'yellow')
+    printColored("Failed -> %i" % TransactionRecord['failed'], 'red')
+
+
+def printColored(Text, Color, end='\n'):
+    print("%s %s %s" % ( fg(Color), Text, fg('white') ), end=end)
+
+
+def showSessionProfit(sessionProfitPercent):
+    _color = 'red' if sessionProfitPercent < 0 else 'green'
+    printColored("Session Profit: %.3f%%" %
+                 (sessionProfitPercent), _color)
+
+
+def showCotations(buy_value, sell_value, Labels=['BUY', 'SELL']):
+    if buy_value:
+        DIFFERENCE = abs((sell_value - buy_value)/ buy_value*100)
+    else:
+        DIFFERENCE = 0
+    print("%s: %.4f  %s: %.4f    DIFF: %.6f%%" % (Labels[0],
+                                                  buy_value,
+                                                  Labels[1],
+                                                  sell_value,
+                                                  DIFFERENCE))
